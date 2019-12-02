16a6a87dcc54745c217777622c1cd7a21b6b4c67
Ryan Wilson <rwilson@ucvts.org>
2019-12-02 08:46:47 -0500

    added starter code

---
+++ js/shapes.js
@@ -0,0 +1,51 @@
+window.onload = function() {
+    // add event listeners to respond to button clicks
+}
+
+/*
+ * Exercise 1.
+ */
+
+const sayHello = function() {
+
+};
+
+/*
+ * Exercise 2.
+ */
+
+const drawRectangle = function() {
+    
+};
+
+/*
+ * Exercise 3.
+ */
+
+const drawColoredRectangle = function() {
+    
+};
+
+/*
+ * Exercise 4.
+ */
+
+const drawTriangle = function() {
+    
+};
+
+/*
+ * Exercise 5.
+ */
+
+const drawFace = function() {
+    
+};
+
+/*
+ * Exercise 6 (extra credit).
+ */
+
+const drawPyramid = function() {
+    
+};
