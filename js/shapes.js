0beec75ba69f8d31d5b121ab9ed15e8c9236aecb
Ryan Wilson <rwilson@ucvts.org>
2019-12-03 10:19:18 -0500

    added helpful hints

--- js/shapes.js
+++ js/shapes.js
@@ -1,5 +1,24 @@
 window.onload = function() {
-    // add event listeners to respond to button clicks
+    document.getElementById("hello-sample").onclick = sayHelloStaff;
+    document.getElementById("rectangle-sample").onclick = drawRectangleStaff;
+    document.getElementById("colored-rectangle-sample").onclick = drawColoredRectangleStaff;
+    document.getElementById("triangle-sample").onclick = drawTriangleStaff;
+    document.getElementById("smile-sample").onclick = drawFaceStaff;
+    document.getElementById("pyramid-sample").onclick = drawPyramidStaff;
+
+    // this is how we're connecting our buttons to our JavaScript functions. let's walk through it.
+    //
+    // document.getElementById("some-id")   <-- you need to give each button a unique ID
+    //                                          and access it in this manner
+    //
+    // onclick is an event that is fired when you click something (in our case, a button).
+    // when we give onclick a value, we're telling JavaScript what to do when we click the button.
+    // you should set onclick equal to your function names (i.e., sayHello).
+    //
+    // there are six event listeners being added for the staff solutions. you'll have an
+    // equivalent set of six event listeners for your solutions. the first one is done for you.
+
+    this.document.getElementById("hello").onclick = sayHello;
 }
 
 /*
@@ -7,7 +26,7 @@ window.onload = function() {
  */
 
 const sayHello = function() {
-
+    // write your exercise 1 code here
 };
 
 /*
@@ -15,7 +34,7 @@ const sayHello = function() {
  */
 
 const drawRectangle = function() {
-    
+    // write your exercise 2 code here
 };
 
 /*
@@ -23,7 +42,7 @@ const drawRectangle = function() {
  */
 
 const drawColoredRectangle = function() {
-    
+    // write your exercise 3 code here
 };
 
 /*
@@ -31,7 +50,7 @@ const drawColoredRectangle = function() {
  */
 
 const drawTriangle = function() {
-    
+    // write your exercise 4 code here
 };
 
 /*
@@ -39,7 +58,7 @@ const drawTriangle = function() {
  */
 
 const drawFace = function() {
-    
+    // write your exercise 4 code here
 };
 
 /*
@@ -47,5 +66,5 @@ const drawFace = function() {
  */
 
 const drawPyramid = function() {
-    
+    // write your exercise 5 code here
 };
