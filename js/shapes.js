ff43fb09065b49639c6f14aff6d83023c9f1c0fa
divanov2004 <55397915+divanov2004@users.noreply.github.com>
2019-12-11 10:35:36 -0500

    1st exercise

--- js/shapes.js
+++ js/shapes.js
@@ -31,7 +31,24 @@ window.onload = function() {
  */
 
 const sayHello = function() {
-    // write your exercise 1 code here
+  const canvas = document.getElementById('student-canvas-1');
+  const context = canvas.getContext('2d');
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+do {
+  var messagePrompt = prompt("messagePrompt: ")
+  if (messagePrompt == null) {
+    break;
+  }
+  if (messagePrompt.length > 50) {
+    alert("Your messagePrompt is too long. Keep it under 50 characters")
+  }
+} while(messagePrompt.length > 50)
+
+if (messagePrompt != null) {
+  context.font = "48px sans-serif";
+  context.strokeText(messagePrompt, 30, 70, 994);
+}
 };
 
 /*
